a82745a759d32d1fea9b8519de6a08b371ce2c11
R3N-di <rendiramadhanmars@gmail.com>
2023-04-02 21:30:21 +0700

    Membuat Layout dasar

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -9,8 +9,18 @@
     </head>
 
     <body>
-        @include('component.pesan')
-        @yield('konten')
+        <div class="container-fluid p-0">
+            @include('component.pesan')
+            <div class="d-flex">
+                <div class="sidebar">
+                    @include('component.sidebar')
+                </div>
+                <div>
+                    @yield('konten')
+                </div>
+            </div>
+        </div>
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     </body>
 </html>
