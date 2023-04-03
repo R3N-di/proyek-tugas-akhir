669cce7170076f5335d51bc481265e28fad1dd38
R3N-di <rendiramadhanmars@gmail.com>
2023-04-03 20:16:07 +0700

    Mengganti nama dan judul

--- resources/views/component/sidebar.blade.php
+++ resources/views/component/sidebar.blade.php
@@ -1,38 +1,38 @@
 <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sticky-top" style="width: 250px; height: 580px;">
     <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
       <svg class="bi pe-none me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-      <span class="fs-4">Sidebar</span>
+      <span class="fs-4">Web Absen</span>
     </a>
     <hr>
     <ul class="nav nav-pills flex-column mb-auto">
       <li class="nav-item">
         <a href="#" class="nav-link active" aria-current="page">
-          <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#home"/></svg>
-          Home
+          {{-- <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#home"/></svg> --}}
+          Absen
         </a>
       </li>
       <li>
         <a href="#" class="nav-link text-white">
-          <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#speedometer2"/></svg>
-          Dashboard
+          {{-- <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#speedometer2"/></svg> --}}
+          Absen Kelas
         </a>
       </li>
       <li>
         <a href="#" class="nav-link text-white">
-          <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#table"/></svg>
-          Orders
+          {{-- <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#table"/></svg> --}}
+          Guru
         </a>
       </li>
       <li>
         <a href="#" class="nav-link text-white">
-          <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#grid"/></svg>
-          Products
+          {{-- <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#grid"/></svg> --}}
+          Siswa
         </a>
       </li>
       <li>
         <a href="#" class="nav-link text-white">
-          <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#people-circle"/></svg>
-          Customers
+          {{-- <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#people-circle"/></svg> --}}
+          Jadwal Pelajaran
         </a>
       </li>
     </ul>
