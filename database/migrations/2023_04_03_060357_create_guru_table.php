76407596633027cf9cb96a8ed417844a960c1a23
R3N-di <rendiramadhanmars@gmail.com>
2023-04-03 13:11:42 +0700

    membuat migration table siswa, guru

---
+++ database/migrations/2023_04_03_060357_create_guru_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('guru', function (Blueprint $table) {
+            $table->string('id', 11)->unique();
+            $table->integer('nip', 18);
+            $table->string('nama', 50);
+            $table->string('password', 10);
+            $table->enum('jk', ['L','P']);
+            $table->string('gambar', 5);
+            $table->integer('nip', 18);
+            $table->integer('nip', 18);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('guru');
+    }
+};
