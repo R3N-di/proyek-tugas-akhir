ae2ad6b94210986b99006bfe48ab9e1d433cfca8
tjakra <cakrasatria01@gmail.com>
2023-04-08 16:15:04 +0700

    edit guru and add siswa

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -58,6 +58,53 @@ class SiswaController extends Controller
         ];
 
         // if($request->has('gambar'))
+        
+        $faker= Faker::create(id_ID);
+        $request->validate([
+            'nis' => 'required|numeric|size:8',
+            'nama' => 'required|string',
+            'jk' => 'required',
+        ], [
+            'nis.required' => 'NIS Harus Diisi',
+            'nis.numeric' => 'NIS Harus Berupa Angka',
+            'nip.size' => 'NIS Harus Memiliki 8 Angka',
+            'nama.required' => 'Nama Harus Diisi',
+            'nama.string' => 'Nama Harus Berbentuk Huruf',
+            'jk.required' => 'Jenis Kelamin Harus Diisi',
+        ]);
+
+        $data = [
+            'id' => $faker->regexify('[A-Z]{11}'),
+            'nis' => $request->nis,
+            'nama' => $request->nama,
+            'password' => $faker->regexify('[A-Z]{10}'),
+            'jk' => $request->jk,
+            'idkelas' => $request->idkelas,
+            'idjurusan' => $request->idjurusan
+
+        ];
+
+        // if($request->has('gambar'))
+        if($request->has('gambar')){
+            $request->validate([
+                'gambar' => 'mimes:png,jpg,jpeg'
+            ], [
+                'gambar.mimes' => 'Gambar harus ber-format : PNG | JPG | JPEG'
+            ]);
+
+            $file_gambar = $request->file('gambar');
+            $gambar_ekstensi = $file_gambar->extension();
+            $gambar_nama = date('ymdhis') . '.' . $gambar_ekstensi;
+            $file_gambar->move(public_path('gambar'), $gambar_nama);
+
+            $data['gambar'] = $gambar_nama;
+        }
+        else{
+            $data['gambar'] = "default_gambar.png";
+        }
+
+        Siswa::create($data);
+        return redirect('/siswa')->withInfo('Berhasil menambah Data');
 
     }
 
