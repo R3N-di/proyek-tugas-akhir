31d1a943ee99695d4f05001754a23f33688dbeec
R3N-di <rendiramadhanmars@gmail.com>
2023-04-08 19:45:32 +0700

    Memprogress pembuatan absen

--- app/Http/Controllers/AbsenController.php
+++ app/Http/Controllers/AbsenController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class AbsenController extends Controller
 {
-    //
+    public function absen_siswa(){
+        return view('page.absen.siswa');
+    }
+
+    public function absen_guru(){
+        return view('page.absen.guru');
+    }
 }
--- resources/views/page/absen/guru.blade.php
+++ resources/views/page/absen/guru.blade.php
@@ -1,6 +1,21 @@
 @extends('layout/app')
 
 @section('konten')
+<div class="d-flex justify-content-between">
+    <div>
+        <label for="idmapel" class="form-label">Mapel :</label>
+        <select name="idmapel" id="idmapel">
+            <option value="Coba">Coba</option>
+            <option value="Coba2">Coba2</option>
+        </select>
+    </div>
+    <div>
+        <form class="d-flex" action="/absen" method="post">
+            <input class="form-control" type="search" name="cari">
+            <a class="btn btn-primary" href="#" role="button">Cari</a>
+        </form>
+    </div>
+</div>
 <table class="table">
     <thead>
       <tr>
@@ -12,11 +27,9 @@
       </tr>
     </thead>
     <tbody>
-        @foreach ( $dataSiswa as $data )
         <tr>
             <th scope="row">1</th>
         </tr>
-        @endforeach
     </tbody>
 </table>
 @endsection
