42c18dc9d6604d8ab937bcb705efc0f633241f85
MunimSudrajat <abdulmunim905@gmail.com>
2023-04-12 21:42:13 +0700

    mengubah field idguru di guru seeder

--- database/seeders/GuruSeeder.php
+++ database/seeders/GuruSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Faker\Factory as Faker;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class GuruSeeder extends Seeder
@@ -18,7 +19,7 @@ class GuruSeeder extends Seeder
         for($i=1;$i<=5;$i++){
             $password = $faker->regexify('[A-Z]{10}');
             DB::table('guru')->insert([
-                "id"=>$faker->regexify('[A-Z]{11}'),
+                "idguru"=>$faker->regexify('[A-Z]{11}'),
                 "nip"=>$faker->numerify('##################'),
                 "nama"=>$faker->name(),
                 "password"=>Hash::make($password),
