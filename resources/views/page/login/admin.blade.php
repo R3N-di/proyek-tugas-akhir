c240504a5225b02efbf4077fc476fac7597e1d4b
tjakra <cakrasatria01@gmail.com>
2023-05-22 23:39:41 +0700

    desain login

--- resources/views/page/login/admin.blade.php
+++ resources/views/page/login/admin.blade.php
@@ -4,24 +4,52 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login</title>
+    <style>
+        body{
+            background-image: url("fileWeb/background2.png");
+            background-size: 150%;
+            background-position: 40%;
+            background-repeat: no-repeat;
+        }
+    </style>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   </head>
   <body>
+    <nav class="navbar bg-light mb-5">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+            <img src="{{ url('fileWeb/logo2.png') }}" alt="Bootstrap" height="35">
+            </a>
+        </div>
+    </nav>
     <div class="container p-5">
-        @include('component.pesan')
-        <a class="btn btn-secondary mb-5" href="beranda/" role="button"><< Kembali</a>
-        <form action="/login" method="post">
-            @csrf
-            <div class="mb-3">
-                <label for="email" class="form-label">Email</label>
-                <input type="text" class="form-control" name="email" placeholder="Masukan Email...">
+        <!-- @include('component.pesan') -->
+        <div class="card bg-transparent bg-gradient ms-4 me-4">
+            <div class="card-header bg-light">
+                <h2 class="card-tittle" align="center">Login Amdin</h2>
             </div>
-            <div class="mb-3">
-              <label for="password" class="form-label">Password</label>
-              <input type="text" class="form-control" name="password" placeholder="Masukan Password...">
+            <div class="card-body text-light">
+                <form action="/login" method="post">
+                    <!-- @csrf -->
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="text" class="form-control" name="email" placeholder="Masukan email...">
+                    </div>
+                    <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="text" class="form-control" name="password" placeholder="Masukan Password...">
+                    </div>
+                    <div class="direction row position-relative pt-5">
+                        <div class="col-6 mb-2">
+                            <button type="submit" class="btn btn-primary bg-gradient float-end">Login</button>
+                        </div>
+                        <div class="col-6">
+                            <a class="btn btn-warning bg-gradient" href="beranda" role="button">Kembali</a> 
+                        </div>
+                    </div>
+                </form>
             </div>
-            <button type="submit" class="btn btn-primary">Login</button>
-        </form>
+        </div>
     </div>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
